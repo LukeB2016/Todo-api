8c0977f3504a7db381a56ee1884e5815b0a1831b
Lucas B <lsantos2061@gmail.com>
2017-12-18 11:52:07 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+//   name: 'Andrew',
+//   age: 21
+// };
+//
+// function updatePerson (obj) {
+//   // obj = {
+//   //   name: 'Andrew',
+//   //   age: 24
+//   // };
+//   obj.age = 24;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [15, 37];
+
+function addGrade (grade) {
+    grades.push(grade);
+}
+console.log(grades)
+addGrade(22);
+console.log(grades)
